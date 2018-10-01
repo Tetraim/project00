cf8ad7969031506424865c02c735a952c0f0c9ee
Kirill <bkdevruby@gmail.com>
2018-10-01 14:06:49 +0300

    migrate to npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users-spa';
+  users = [{
+		name:'users',
+		email:'test@test.com',
+		role:'admin'
+	},
+	{
+		name:'1',
+		email:'1',
+		role:'1'
+
+	},
+	{
+		name:'2',
+		email:'2',
+		role:'2'
+	}
+	]
+  constructor() { console.log('123123123') }
 }
+
+
+// export class HeroesComponent implements OnInit {
+ 
+//   heroes = HEROES;
+//   selectedHero: Hero;
+ 
+  
+ 
+//   ngOnInit() {
+//   }
+ 
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//   }
+// }
